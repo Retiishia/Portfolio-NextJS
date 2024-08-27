ffd8373e1b423738eed491fc6b91f306929554cf
Retiishia <azizfarhan72@gmail.com>
2024-08-28 03:39:47 +0700

    Add : Adding OpenGraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,30 @@
 import './globals.css'
 
+export const metadata = {
+  title: "Retiishia",
+  description: 'Welcome to my portfolio!',
+  openGraph: {
+    title: 'Retiishia',
+    description: 'Welcome to my portfolio!',
+    siteName: 'retiishia.dev',
+    images: [
+      {
+        url: "/screenshot.png"
+      }
+    ],
+    locale: 'en-GB',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Retiishia',
+    description: 'Welcome to my portfolio!',
+    creator: '@Retiishia',
+    images: ['/screenshot.png'],
+  },
+  category: 'technology',
+}
+
 export default function RootLayout({
   children,
 }: {

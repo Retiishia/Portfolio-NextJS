dafa2488f57aa99cb23d6f31de642fec25534392
Retiishia <azizfarhan72@gmail.com>
2024-08-29 17:26:45 +0700

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,7 @@ export const metadata = {
     siteName: 'Retiishia ',
     images: [
       {
-        url: '/screenshot.png',
-        width: 800,
-        height: 600,
+        url: '/screenshot.png'
       },
     ],
     locale: 'en-GB',

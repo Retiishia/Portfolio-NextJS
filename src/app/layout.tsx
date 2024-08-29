26a666030ecda4d6c5a02acddfd65bf2988e67e2
Retiishia <azizfarhan72@gmail.com>
2024-08-29 17:31:53 +0700

    fix: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,34 @@
+import { url } from 'inspector'
 import './globals.css'
 
 export const metadata = {
-  title: 'Retiishia',
-    description: 'Welcome to My Portofolio',
+  title: "Retiishia",
+    description: 'Welcome to my portfolio!',
+    siteName: 'retiishia.my.id',
     url: 'https://retiishia.my.id',
-    siteName: 'Retiishia ',
     images: [
       {
-        url: '/screenshot.png'
-      },
+        url: "/screenshot.png"
+      }
     ],
     locale: 'en-GB',
     type: 'website',
   }
 
+
+// export const metadata = {
+//   title: 'Retiishia',
+//     description: 'Welcome to My Portofolio',
+//     url: 'https://retiishia.my.id',
+//     siteName: 'retiishia.my.id ',
+//     images: [
+//       {
+//         url: '/screenshot.png'
+//       },
+//     ],
+//     locale: 'en-GB',
+//     type: 'website',
+//   }
 
 export default function RootLayout({
   children,

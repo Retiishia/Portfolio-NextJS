8e08d965a85542a6adc03dc7089714bca3e0b983
Retiishia <azizfarhan72@gmail.com>
2024-08-31 22:00:11 +0700

    fix: fixing navbar hamburger

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { ButtonLink } from './Scroller';
-import { FaDiscord, FaGit, FaGithub } from 'react-icons/fa';
+import { FaDiscord, FaGithub } from 'react-icons/fa';
 
 const Navbar: React.FC = () => {
 
@@ -30,15 +30,15 @@ const Navbar: React.FC = () => {
   return (
     <header className={`fixed w-full bg-transparent z-50 mnhdr h-auto transition-all ${scrolling ? 'blur-background' : ''} ${mobileMenuOpen ? 'h-24 blur-background' : ''}`}>
       <nav className="container mx-auto px-6 py-3 flex justify-between items-center">
-        <Link href="/" className="text-2xl font-semibold  text-white animated-gradient">
+        <Link href="/" className="text-2xl font-semibold animated-gradient">
           Retiishia
         </Link>
         <div className="hidden md:flex space-x-6">
-          <ButtonLink className="text-lg font-medium  text-white hover:text-cyan-300" buttonText='Home' elementId='hero'/>
-          <ButtonLink className="text-lg font-medium  text-white hover:text-cyan-300" buttonText='About' elementId='about'/>
-          <ButtonLink className="text-lg font-medium  text-white hover:text-cyan-300" buttonText='Experience' elementId='experience'/>
-          <ButtonLink className="text-lg font-medium  text-white hover:text-cyan-300" buttonText='Project' elementId='project'/>
-          <ButtonLink className="text-lg font-medium  text-white hover:text-cyan-300" buttonText='Contact' elementId='contact'/>
+          <ButtonLink className="text-lg font-medium  text-white hover:text-cyan-400" buttonText='Home' elementId='hero'/>
+          <ButtonLink className="text-lg font-medium  text-white hover:text-cyan-400" buttonText='About' elementId='about'/>
+          <ButtonLink className="text-lg font-medium  text-white hover:text-cyan-400" buttonText='Experience' elementId='experience'/>
+          <ButtonLink className="text-lg font-medium  text-white hover:text-cyan-400" buttonText='Project' elementId='project'/>
+          <ButtonLink className="text-lg font-medium  text-white hover:text-cyan-400" buttonText='Contact' elementId='contact'/>
         </div>
         <div className="flex space-x-4">
           <button
@@ -57,14 +57,32 @@ const Navbar: React.FC = () => {
               <path d="M4 6h16M4 12h16M4 18h16"></path>
             </svg>
           </button>
-          <Link href="https://github.com/Retiishia" target='_blank' className="hover:text-cyan-400 hidden md:flex">
+          <Link href="https://github.com/Retiishia" target='_blank' className="hidden md:flex hover:text-cyan-400">
             <FaGithub size={36}/>
           </Link>
-          <Link href="https://discord.gg/zpcWqf2B8k" target='_blank' className="hover:text-cyan-400 hidden md:flex">
+          <Link href="https://discord.gg/zpcWqf2B8k" target='_blank' className="hidden md:flex hover:text-cyan-400">
             <FaDiscord size={36}/>
           </Link>
         </div>
       </nav>
+      <div className={`md:hidden ${mobileMenuOpen ? 'flex' : 'hidden'} justify-between mt-4 mb-6 ml-5`}>
+        <div className="flex flex-col space-y-4">
+          <ButtonLink className="text-left text-lg font-medium  text-white hover:text-cyan-400" buttonText='Home' elementId='hero'/>
+          <ButtonLink className="text-left text-lg font-medium  text-white hover:text-cyan-400" buttonText='About' elementId='about'/>
+          <ButtonLink className="text-left text-lg font-medium  text-white hover:text-cyan-400" buttonText='Experience' elementId='experience'/>
+          <ButtonLink className="text-left text-lg font-medium  text-white hover:text-cyan-400" buttonText='Project' elementId='project'/>
+          <ButtonLink className="text-left text-lg font-medium  text-white hover:text-cyan-400" buttonText='Contact' elementId='contact'/>
+
+        </div>
+        <div className="flex items-center mt-20 mr-4 ">
+          <Link href="https://github.com/Retiishia" target='_blank' className="ml-4 hover:text-cyan-400">
+            <FaGithub  size={36}/>
+          </Link>
+          <Link href="https://discord.gg/zpcWqf2B8k" target='_blank' className="ml-4 hover:text-cyan-400">
+            <FaDiscord  size={36}/>
+          </Link>
+        </div>
+      </div>
     </header>
   );
 };
